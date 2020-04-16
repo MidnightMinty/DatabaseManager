ffe1157e4c84d01582fcf8981e27af5432e23d8f
MidnightMinty <46412610+MidnightMinty@users.noreply.github.com>
2020-04-15 21:47:53 -0400

    Create DatabaseManager.java

---
+++ SQLJAVA/src/javaSQL/DatabaseManager.java
@@ -0,0 +1,10 @@
+package javaSQL;
+
+public class DatabaseManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this will be where all the code for modifying the database
+	}
+
+}
